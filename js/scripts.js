ca16bc067196cea23ff6ba5054ee0a07c89d1cb2
Jacob Estep <jacobwestep@gmail.com>
2023-01-14 12:50:46 -0800

    home page changes, some work on about page

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 let bkg = document.getElementById('bkg');
-let lamp = document.getElementById("lamp_button");
+let lamp = document.getElementById('lamp_button');
 let night = false;
 
-function changeBkg(buttonHovered){
-    //bkg.style.backgroundColor = "transparent"
-    if(buttonHovered == 0){
-        bkg.style.backgroundImage = "var(--lamp-hero)";
-    }else if(buttonHovered == 1){
-        bkg.style.backgroundImage = "var(--whackamole-hero)";
-    }else if(buttonHovered == 2){
-        bkg.style.backgroundImage = "var(--bentwo-hero)";
-    }else if(buttonHovered == 3){
-        bkg.style.backgroundImage = "var(--ecommerce-hero)";
-    }else if(buttonHovered == 4){
-        bkg.style.backgroundImage = "var(--watch-hero)";
-    }else if(buttonHovered == 5){
-        bkg.style.backgroundImage = "var(--honeycomb-hero)";
-    }
+function changeBkg(buttonHovered) {
+  //bkg.style.backgroundColor = "transparent"
+  // if(buttonHovered == 0){
+  //     bkg.style.backgroundImage = "var(--lamp-hero)";
+  // }else if(buttonHovered == 1){
+  //     bkg.style.backgroundImage = "var(--whackamole-hero)";
+  // }else if(buttonHovered == 2){
+  //     bkg.style.backgroundImage = "var(--bentwo-hero)";
+  // }else if(buttonHovered == 3){
+  //     bkg.style.backgroundImage = "var(--ecommerce-hero)";
+  // }else if(buttonHovered == 4){
+  //     bkg.style.backgroundImage = "var(--watch-hero)";
+  // }else if(buttonHovered == 5){
+  //     bkg.style.backgroundImage = "var(--honeycomb-hero)";
+  // }
 }
 
-function resetBkg(){
-    bkg.style.backgroundImage = "none";
+function resetBkg() {
+  // bkg.style.backgroundImage = "none";
 }
 
-lamp.addEventListener("mouseenter", changeBkg("lamp"));
+lamp.addEventListener('mouseenter', changeBkg('lamp'));
 
-lamp.addEventListener("mouseleave", resetBkg());
+lamp.addEventListener('mouseleave', resetBkg());
