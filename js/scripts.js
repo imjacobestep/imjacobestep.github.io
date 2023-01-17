9d8eb7d246291d66960c89bdb0531b93d0773849
Jacob Estep <jacobwestep@gmail.com>
2023-01-16 23:38:00 -0800

    reworked projects, dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,16 @@
-let bkg = document.getElementById('bkg');
-let lamp = document.getElementById('lamp_button');
-let night = false;
+matcher = window.matchMedia('(prefers-color-scheme: dark)');
+matcher.addListener(onUpdate);
+onUpdate();
 
-function changeBkg(buttonHovered) {
-  //bkg.style.backgroundColor = "transparent"
-  // if(buttonHovered == 0){
-  //     bkg.style.backgroundImage = "var(--lamp-hero)";
-  // }else if(buttonHovered == 1){
-  //     bkg.style.backgroundImage = "var(--whackamole-hero)";
-  // }else if(buttonHovered == 2){
-  //     bkg.style.backgroundImage = "var(--bentwo-hero)";
-  // }else if(buttonHovered == 3){
-  //     bkg.style.backgroundImage = "var(--ecommerce-hero)";
-  // }else if(buttonHovered == 4){
-  //     bkg.style.backgroundImage = "var(--watch-hero)";
-  // }else if(buttonHovered == 5){
-  //     bkg.style.backgroundImage = "var(--honeycomb-hero)";
-  // }
+lightSchemeIcon = document.querySelector('link#light-icon');
+darkSchemeIcon = document.querySelector('link#dark-icon');
+
+function onUpdate() {
+  if (matcher.matches) {
+    lightSchemeIcon.remove();
+    document.head.append(darkSchemeIcon);
+  } else {
+    document.head.append(lightSchemeIcon);
+    darkSchemeIcon.remove();
+  }
 }
-
-function resetBkg() {
-  // bkg.style.backgroundImage = "none";
-}
-
-lamp.addEventListener('mouseenter', changeBkg('lamp'));
-
-lamp.addEventListener('mouseleave', resetBkg());
